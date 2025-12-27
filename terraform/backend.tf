2aafef9b8b69b46938a2cdac81eae86968666622
Bosun Olisa <61821794+olisabosun@users.noreply.github.com>
2025-12-28 00:49:38 +0100

    Disable S3 backend configuration in backend.tf
    Comment out backend configuration settings in Terraform.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
     # Configure these values via backend config file or CLI
-     bucket         = "bosun-staging-bucket"
-     key            = "serverless-health-check/terraform.tfstate"
-     region         = "us-east-1"
-     dynamodb_table = "terraform-state-lock"
-     encrypt        = true
+    # bucket         = "bosun-staging-bucket"
+    # key            = "serverless-health-check/terraform.tfstate"
+    # region         = "us-east-1"
+    # dynamodb_table = "terraform-state-lock"
+    # encrypt        = true
   }
 }
